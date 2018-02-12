67912e3015cc1cc5bcd673c7e7a8fa8f5a8db521
lyx <563150079@qq.com>
2018-02-12 16:03:55 -0500

    2.12

--- assignment1/part2.py
+++ assignment1/part2.py
@@ -1,11 +1,6 @@
 from openpyxl import load_workbook
 
-wb = load_workbook(filename='data1.xlsx')
-
-ws = wb['Sheet1']
-
-# for row in ws.rows:
-#     print(row[0].value)
+import math
 
 
 class lenearRegression(object):
@@ -17,18 +12,24 @@ class lenearRegression(object):
         pass
 
     def loadData(self, file, sheet):
+        """
+        loading raw data
+        """
         wb = load_workbook(filename=file)
         self.ws = wb[sheet]
-        self.rows = self.ws.rows
-        self.max_row = ws.max_row
-        return ws
+        self.max_row = self.ws.max_row
+        self.setFactor()
 
-    def gradientDescent(self):
-
+    def learning(self):
+        """
+        iterate each attributes for gradient descent
+        """
         for j in range(len(self.theta)):
             self.theta[j] = self.batchGD(j, self.theta[j])
 
     def batchGD(self, j, theta):
+        """
+        """
         total = 0
         alpha = 1
         minValue = theta
@@ -36,17 +37,21 @@ class lenearRegression(object):
             row_ = self.reOrderValue(row)
             err = self.error(row_)
             total += err * row_[j]
-        # print(j)
+
         jtheta = (total / self.max_row) * alpha
 
+        # print(self.theta[j])
         if minValue <= minValue - jtheta:
             return minValue
         else:
             self.theta[j] = minValue - jtheta
-            return self.batchGD(j, self.theta[j])
+            return self.batchGD(j, self.theta[j])  # recursion for min value
 
     def error(self, row):
-
+        """
+        calculate the distance between
+        computing result and real result
+        """
         y = self.theta[0] * row[0] + \
             self.theta[1] * row[1] + \
             self.theta[2] * row[2] + \
@@ -58,15 +63,46 @@ class lenearRegression(object):
     def reOrderValue(self, row):
         r = []
         r.append(1)
-        r.append(row[1].value)
+        r.append(self.factorScaling(row[1].value))
         r.append(row[3].value)
         r.append(row[4].value)
         r.append(row[5].value)
         r.append(row[2].value)
         return r
 
+    def factorScaling(self, num):
+        """
+        scaling range for large number
+        """
+        return (num - self.meanVal) / self.sd
+
+    def setFactor(self):
+        """
+        Generate mean and stand deviation
+        for number in large range
+        """
+        val = [row[1].value for row in self.ws.rows]
+        self.meanVal = sum(val) / len(val)
+        powTotal = 0
+
+        for i in val:
+            powTotal += pow(i - self.meanVal, 2)
+        self.sd = math.sqrt(powTotal / len(val))
+
+    def test(self):
+
+        for row in self.ws.rows:
+            y = self.theta[0] * 1 + \
+                self.theta[1] * self.factorScaling(row[1].value) + \
+                self.theta[2] * row[3].value + \
+                self.theta[3] * row[4].value + \
+                self.theta[4] * row[5].value
+            print(row[2].value - y)
+
 
 l = lenearRegression()
-ws = l.loadData('data1.xlsx', 'Sheet1')
-l.gradientDescent()
+l.loadData('Data.xlsx', 'Sheet1')
+l.learning()
+# l.test()
 print(l.theta)
+# print(l.sd)
--- assignment1/test.py
+++ assignment1/test.py
@@ -1,1 +1,17 @@
-a = [1, 2, 3, 4, 5]
+from openpyxl import load_workbook
+
+import math
+
+theta = [-21.77230885416671, 1, -1324.5454118154291, -1, -2]
+
+wb = load_workbook(filename='Data.xlsx')
+ws = wb['Sheet1']
+
+for row in ws.rows:
+
+    y = theta[0] * 1 + \
+        theta[1] * row[1].value + \
+        theta[2] * row[3].value + \
+        theta[3] * row[4].value + \
+        theta[4] * row[5].value
+    print(row[2].value - y)
