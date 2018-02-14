17959935a133764f80d1979069553f0cb2e59565
lyx <563150079@qq.com>
2018-02-14 17:13:39 -0500

    2.14

--- assignment1/part2.py
+++ assignment1/part2.py
@@ -1,16 +1,17 @@
 from openpyxl import load_workbook
-
+import copy
 import math
 import random
 
 
-class lenearRegression(object):
+class linearRegression(object):
     """docstring for LenearRegression"""
 
     def __init__(self):
-        self.theta = [0, 1, 2, -1, -2]
+        self.theta = [0, 0, 0, 0, 0]
         self.mean = [0, 0, 0, 0]
         self.sd = [0, 0, 0, 0]
+        self.dataSet = []
         self.trainSet = []
         self.testSet = []
 
@@ -20,45 +21,52 @@ class lenearRegression(object):
         """
         wb = load_workbook(filename=file)
         self.ws = wb[sheet]
-        self.max_row = self.ws.max_row
-        self.setFactor()
+        # self.setFactor()
+        for row in self.ws.rows:
+            r = []
+            r.append(1)
+            r.append(row[2].value)
+            r.append(row[3].value)
+            r.append(row[4].value)
+            r.append(row[5].value)
+            r.append(row[1].value)
+            self.dataSet.append(r)
+
+        self.max_row = len(self.dataSet)
 
     def learning(self):
         """
         iterate each attributes for gradient descent
         """
-        minCF = self.costFunction(self.theta)
-        for j in range(len(self.theta)):
-            self.theta[j] = self.batchGD(j, [0, 1, 2, -1, -2], minCF)
+        lastCF = 0
+        CF = self.costFunction(self.theta)
+        while abs(lastCF - CF) > 0.1:
+            self.batchGD(CF)
+            lastCF = CF
+            CF = self.costFunction(self.theta)
+            print(self.theta, lastCF, CF)
 
-    def batchGD(self, j, thetaList, minCF):
+    def batchGD(self, minCF):
         """
         """
-        th = thetaList[j]
+        # print(thetaList)
+        localTheta = self.theta[:]
         total = 0
-        alpha = 1
-        for row in self.ws.rows:
-            row_ = self.reOrderValue(row)
-            err = self.error(row_, thetaList)
-            total += err * row_[j]
-        # print(total)
-        thetaList[j] = thetaList[j] - (total / self.max_row) * alpha
-
-        cf = self.costFunction(thetaList)
-
-        # print(j, minCF, cf)
-        if minCF <= cf:
-            return th
-        else:
-            # recursion for min value
-            return self.batchGD(j, thetaList, cf)
+        alpha = 0.0001
+        for j in range(len(self.theta)):
+            for row in self.dataSet[:40000]:
+                # row_ = self.reOrderValue(row)
+                err = self.error(row, localTheta)
+                total += err * row[j]
+            self.theta[j] = self.theta[j] - (total / self.max_row) * alpha
 
     def costFunction(self, tl):
         total = 0
-        for row in self.ws.rows:
-            row_ = self.reOrderValue(row)
-            err = self.error(row_, tl)
+
+        for row in self.dataSet[:40000]:
+            err = self.error(row, tl)
             total += pow(err, 2)
+
         return total / (2 * self.max_row)
 
     def error(self, row, t):
@@ -66,66 +74,39 @@ class lenearRegression(object):
         calculate the distance between
         computing result and real result
         """
+
         y = t[0] * row[0] + \
             t[1] * row[1] + \
             t[2] * row[2] + \
             t[3] * row[3] + \
             t[4] * row[4]
-        # print(y - row[5])
         return y - row[5]
 
     def reOrderValue(self, row):
         r = []
-        r.append(1)
-        r.append(self.factorScaling(row[1].value, 0))
-        r.append(self.factorScaling(row[3].value, 1))
-        r.append(self.factorScaling(row[4].value, 2))
-        r.append(self.factorScaling(row[5].value, 3))
-        r.append(row[2].value)
+        r.append(1)  # const
+        r.append(row[2].value)  # temp
+        r.append(row[3].value)  # humidity
+        r.append(row[4].value)  # wind speed
+        r.append(row[5].value)  # vapour pressure
+        r.append(row[1].value)  # MW
         return r
-
-    def factorScaling(self, num, k):
-        """
-        scaling range for large number
-        """
-        return (num - self.mean[k]) / self.sd[k]
-
-    def setFactor(self):
-        """
-        Generate mean and stand deviation
-        for number in large range
-        """
-        val = []
-        val.append([row[1].value for row in self.ws.rows])
-        val.append([row[3].value for row in self.ws.rows])
-        val.append([row[4].value for row in self.ws.rows])
-        val.append([row[5].value for row in self.ws.rows])
-
-        for i in range(len(val)):
-            self.mean[i] = sum(val[i]) / len(val[i])
-
-        for v in range(len(val)):
-            powTotal = 0
-            for i in val[v]:
-                powTotal += pow(i - self.mean[v], 2)
-                self.sd[v] = math.sqrt(powTotal / len(val[v]))
 
     def test(self):
         # print(self.theta)
         total = 0
-        for row in self.ws.rows:
-            y = self.theta[0] * 1 + \
-                self.theta[1] * self.factorScaling(row[1].value, 0) + \
-                self.theta[2] * self.factorScaling(row[3].value, 1) + \
-                self.theta[3] * self.factorScaling(row[4].value, 2) + \
-                self.theta[4] * self.factorScaling(row[5].value, 3)
-            total += abs(y - row[2].value)
+        for row in self.dataSet[40000:]:
+            y = self.theta[0] * row[0] + \
+                self.theta[1] * row[1] + \
+                self.theta[2] * row[2] + \
+                self.theta[3] * row[3] + \
+                self.theta[4] * row[4]
+            total += abs(y - row[5])
         print(total/self.max_row)
 
 
-l = lenearRegression()
+l = linearRegression()
 l.loadData('data.xlsx', 'Sheet1')
+
 l.learning()
 l.test()
-# print(l.mean)
-print(l.sd)
--- assignment1/test.py
+++ assignment1/test.py
@@ -2,16 +2,12 @@ from openpyxl import load_workbook
 
 import math
 
-theta = [-21.77230885416671, 1, -1324.5454118154291, -1, -2]
 
-wb = load_workbook(filename='Data.xlsx')
+wb = load_workbook(filename='data1.xlsx')
 ws = wb['Sheet1']
 
-for row in ws.rows:
+for row in ws.iter_rows(min_row=5, max_row=10):
+    print(row)
 
-    y = theta[0] * 1 + \
-        theta[1] * row[1].value + \
-        theta[2] * row[3].value + \
-        theta[3] * row[4].value + \
-        theta[4] * row[5].value
-    print(row[2].value - y)
+
+[1.2450348399099198, 140.24729686717893, 3.5610502974044036, -14.608456142496287, 51.441167706290436] 72763.55151148335 72763.45160530935
