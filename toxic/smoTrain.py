6d8a568942f1ff5369fa26051f5bf0a82fbc2b03
seraphim-lyx <563150079@qq.com>
2018-03-23 00:11:56 +0800

    3.23

--- toxic/smoTrain.py
+++ toxic/smoTrain.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import random
 
 class smoTrain(object):
     """docstring for smoTrain"""
@@ -15,19 +15,46 @@ class smoTrain(object):
         if z is None:
             z = np.copy(x)
         if self.kernel == 'linear':
-            return np.dot(x, z)
+            return self.linearKernel(x, z)
         elif self.kernel == 'poly':
-            return np.power(np.dot(x, z.T + 1.0), 2)
-        elif self.kernel == 'rbf':
-            xx = np.sum(x * x, axis=1)
-            zz = np.sum(z * z, axis=1)
-            res = - 2.0 * np.dot(x, z.T) + \
-                xx.reshape(-1, 1) + \
-                zz.reshape(1, -1)
-            return np.exp(-self.gamma * res)
+            return self.polyKernel(x, z)
+        elif self.kernel == 'rbf':     #K(x,z)=exp(− gamma*|x−z|**2)
+            return self.GaussianKernel(x, z)
         else:
             print("kernel error")
 
     def train(self, X, y):
         X = np.array(X)
         y = np.array(y)
+        self.alpha = np.zeros(len(y))
+        count = 0
+        while count < 5:
+            alpha_change = 0
+            for i in self.alpha:
+                Ei = np.sum(np.dot(self.alpha*y[i],self._kernel(X,X[i])))-b
+                if():
+                    j = random.randint(0,len(self.alpha))
+                    while i == j:
+                        j = random.randint(0, len(self.alpha))
+                        
+                alpha_change += 1
+
+    def predict(X):
+        pass
+
+    def evaluate(X, y):
+        pass
+
+    def linearKernel(x, z):
+        return np.dot(x, z)
+
+    def polyKernel(x, z):
+        return np.power(np.dot(x, z.T + 1.0), 2)
+
+    def GaussianKernel(x, z):
+        xx = np.sum(x * x, axis=1)
+        zz = np.sum(z * z, axis=1)
+        res = - 2.0 * np.dot(x, z.T) + \
+        xx.reshape(-1, 1) + \
+        zz.reshape(1, -1)
+        return np.exp(-self.gamma * res) # 0< gamma < 1
---
+++ toxic/test.py
@@ -0,0 +1,12 @@
+import numpy as np 
+gamma = 0.02
+x = np.array([[1,2],[3,4],[5,6]])
+z = x
+xx = np.sum(x * x, axis=1)
+zz = np.sum(z * z, axis=1)
+res = - 2.0 * np.dot(x, z.T) + \
+xx.reshape(-1, 1) + \
+zz.reshape(1, -1)
+print(np.exp(-gamma * res))
+
+# print(np.dot(a,b.T))
