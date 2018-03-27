36131e81968a6c8d4cf4663dbfa591960491f460
seraphim-lyx <563150079@qq.com>
2018-03-27 23:41:38 +0800

    3.27.2

--- toxic/test.py
+++ toxic/test.py
@@ -8,11 +8,4 @@ res = - 2.0 * np.dot(x, z.T) + \
 xx.reshape(-1, 1) + \
 zz.reshape(1, -1)
 k = np.exp(-gamma * res)
-# print(k[0])
-alpha = np.array([2,2,3])
-# y = np.array([1,1,2])
-x = np.array([[1,2,4,4,4],[2,3,5,5,5],[3,4,5,5,5]])
-# print(alpha * y * x)
-print(np.dot(alpha, x))
-# print(np.dot(a,b.T))
-# print(hex(65))
+
